365b50f984fde151946c24ac0eac6424772fe8cc
Egidijus Gecius <e.gecius@gmail.com>
2017-08-27 09:43:35 +0100

    add read functionality for a single pet

--- app/src/main/java/db/PetsStoreImpl.java
+++ app/src/main/java/db/PetsStoreImpl.java
@@ -2,14 +2,21 @@ package db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 
 import com.egecius.demo_sqlite.Pet;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static db.PetsContract.PetsEntry.*;
+import static db.PetsContract.PetsEntry.COLLUMN_BREED;
+import static db.PetsContract.PetsEntry.COLLUMN_GENDER;
+import static db.PetsContract.PetsEntry.COLLUMN_ID;
+import static db.PetsContract.PetsEntry.COLLUMN_NAME;
+import static db.PetsContract.PetsEntry.COLLUMN_WEIGHT;
+import static db.PetsContract.PetsEntry.TABLE_NAME;
 
 /**
  * Stores pets information in a database
@@ -19,9 +26,9 @@ class PetsStoreImpl implements PetsStore {
 	private final SQLiteDatabase db;
 
 	public PetsStoreImpl(final Context context) {
-		 db = new PetsDbHelper(context).getWritableDatabase();
+		db = new PetsDbHelper(context).getWritableDatabase();
 	}
-	
+
 	@Override
 	public void add(final Pet pet) {
 		ContentValues values = createContentValues(pet);
@@ -40,7 +47,31 @@ class PetsStoreImpl implements PetsStore {
 
 	@Override
 	public List<Pet> read() {
-		return null;
+		Cursor cursor = db.query(TABLE_NAME, /*all columns*/ null, /* all rows*/ null, null, null,
+				null, null);
+
+		Pet pet = extractPetFromCursor(cursor);
+
+		// TODO: 27/08/2017 return full list
+
+		ArrayList<Pet> petsList = new ArrayList<>();
+		petsList.add(pet);
+
+		return petsList;
+	}
+
+	private Pet extractPetFromCursor(final Cursor cursor) {
+		cursor.moveToFirst();
+
+		int id = cursor.getInt(cursor.getColumnIndexOrThrow(COLLUMN_ID));
+		String name = cursor.getString(cursor.getColumnIndexOrThrow(COLLUMN_NAME));
+		int breed = cursor.getInt(cursor.getColumnIndexOrThrow(COLLUMN_BREED));
+		int gender = cursor.getInt(cursor.getColumnIndexOrThrow(COLLUMN_GENDER));
+		int weight = cursor.getInt(cursor.getColumnIndexOrThrow(COLLUMN_WEIGHT));
+
+		cursor.close();
+
+		return new Pet(id, name, breed, gender, weight);
 	}
 
 	@Override
