9a7c3b2b4d9324a953f5b59f56248f7d23a8a01c
Egidijus Gecius <e.gecius@gmail.com>
2017-08-27 09:30:56 +0100

    add update & remove functions

--- app/src/main/java/db/PetsStoreImpl.java
+++ app/src/main/java/db/PetsStoreImpl.java
@@ -3,6 +3,7 @@ package db;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
 
 import com.egecius.demo_sqlite.Pet;
 
@@ -23,13 +24,18 @@ class PetsStoreImpl implements PetsStore {
 	
 	@Override
 	public void add(final Pet pet) {
+		ContentValues values = createContentValues(pet);
+		db.insert(TABLE_NAME, null, values);
+	}
+
+	@NonNull
+	private ContentValues createContentValues(final Pet pet) {
 		ContentValues values = new ContentValues();
 		values.put(COLLUMN_NAME, pet.name);
 		values.put(COLLUMN_BREED, pet.breed);
 		values.put(COLLUMN_GENDER, pet.gender);
 		values.put(COLLUMN_WEIGHT, pet.weight);
-
-		db.insert(TABLE_NAME, null, values);
+		return values;
 	}
 
 	@Override
@@ -39,14 +45,18 @@ class PetsStoreImpl implements PetsStore {
 
 	@Override
 	public void remove(final Pet pet) {
-
 		String id = String.valueOf(pet.id);
-		db.delete(TABLE_NAME, COLLUMN_ID, id, new S);
-
+		String[] petIdValue = {id};
+		db.delete(TABLE_NAME, COLLUMN_ID, petIdValue);
 	}
 
 	@Override
 	public void update(final Pet pet) {
 
+		ContentValues values = createContentValues(pet);
+
+		String id = String.valueOf(pet.id);
+		String[] petIdValue = {id};
+		db.update(TABLE_NAME, values, COLLUMN_ID, petIdValue);
 	}
 }
